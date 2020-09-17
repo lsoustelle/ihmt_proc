190bb28133cfc26bf3792148274027ad429ae204
Soustelle <lucas.soustelle@gmail.com>
2020-09-17 09:50:35 +0200

    adding headers info

--- process_ihMT.sh
+++ process_ihMT.sh
@@ -4,6 +4,23 @@ trap control_c SIGINT # delete temp folder upon CTRL+C hit
 BASEDIR=$(dirname "$0")
 TOOLSDIR=${BASEDIR}/Tools
 
+
+# ///////////////////////////////////////////////////////////////////////////////////////////
+# //                 Université d’Aix Marseille (AMU) -
+# //                 Centre National de la Recherche Scientifique (CNRS) –
+# //                 Copyright © 2018 AMU, CNRS All Rights Reserved.
+# //
+# //     These computer program listings and specifications, herein, are
+# //     the property of Université d’Aix Marseille and CNRS
+# //     shall not be reproduced or copied or used in whole or in part as
+# //     the basis for manufacture or sale of items without written permission.
+# //     For a license agreement, please contact: <mailto: licensing@sattse.com>
+# //
+# //	 Developed by Lucas Soustelle, PhD, Aix Marseille Univ, CNRS, CRMBM
+# // 	 Mail: lucas.soustelle@univ-amu.fr
+# //////////////////////////////////////////////////////////////////////////////////////////
+
+
 function cleanup
 { 
 	rm -rf ${TMP_FLD} 
